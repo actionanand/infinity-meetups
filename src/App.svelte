7549ec421a5f94e081e7650401e1d4ac5cc76ba0
Anand raja <46064269+actionanand@users.noreply.github.com>
2022-01-28 23:42:10 +0530

    animation complete

--- src/App.svelte
+++ src/App.svelte
@@ -1,64 +1,97 @@
 <script>
-  import meetups from './MeetUps/meetups-store.js';
+  import { fade, fly, slide, scale } from 'svelte/transition';
+  // to remove chumpy effect when we add items
+  import { flip } from 'svelte/animate';
 
-  import Header from './UI/Header.svelte';
-  import MeetupGrid from './MeetUps/MeetupGrid.svelte';
-  import EditMeetup from './MeetUps/EditMeetup.svelte';
-  import MeetupDetail from './MeetUps/MeetupDetail.svelte';
+  import progress from './app-store';
+  import Spring from './Spring.svelte';
+  import { cubicIn } from 'svelte/easing';
 
   export let appName;
 
-  let editMode;
-  let page = 'overview';
-  let pageData = {};
-  let editedId = null;
+  let boxInput;
+  let boxes = [];
+  let showPara = false;
+  let isSpringOn = false;
 
-  // let meetups;
 
-  function onSaveMeetup() {
-    editMode = null;
-    editedId = null;
+  function addBox() {
+    boxes = [boxInput.value, ...boxes];
+    boxInput.value = '';
   }
 
-  function cancelEdit() {
-    editMode = null;
-    editedId = null;
+  function discard(box) {
+    boxes = boxes.filter(el => el !== box)
   }
-
-  function showDetails(event) {
-    page = 'details';
-    pageData.id = event.detail;
-  }
-
-  function closeDetails() {
-    page = 'overview';
-    pageData = {};
-  }
-
-  function onEditMeetup(event) {
-    editMode = 'edit';
-    editedId = event.detail;
-  }
-
 </script>
 
+<div class="title-warap">
+  <h1 class="capitalize-it">{appName}</h1>
+</div>
+
+
+<button on:click="{() => isSpringOn = true}" disabled="{isSpringOn}"> Spring Animation </button>
+
+<progress value="{$progress}"></progress>
+
+{#if isSpringOn}
+<div>
+  <Spring on:close-spring="{() => isSpringOn = false}" />
+</div>
+{:else if !isSpringOn}
+  <div>
+    <button on:click="{() => showPara = !showPara}">Toggle</button>
+    {#if showPara}
+      <p transition:fly="{{x: 300}}">Can you see me?</p>
+      <p in:fade out:fly="{{x: -300}}">I'm visibel, right?</p>
+    {/if}
+
+    <hr>
+
+    <input type="text" bind:this="{boxInput}">
+    <button on:click="{addBox}">Add Box</button>
+
+        <!-- if loacal modifier added, animation will be only for individual items, not when added altogether -->
+    {#if showPara}
+      {#each boxes as box (box)}
+        <div class="box" transition:fly|local="{{easing: cubicIn, opacity: 0, x: 0, y: 300}}"
+          on:click="{discard.bind(this, box)}"
+          on:introstart="{() => console.log('Adding Animation begins')}"
+          on:introend="{() => console.log('Adding animation ends')}"
+          on:outrostart="{() => console.log('Removing Animation begins')}"
+          on:outroend="{() => console.log('Removing animation ends')}"
+          animate:flip="{{duration: 900}}">
+          {box}
+        </div>
+      {/each}
+    {/if}
+  </div>
+{/if}
+
 <style>
-  main {
-    margin-top: 5rem;
+  .title-warap {
+    display: flex;
+    justify-content: center;
   }
 
+  .capitalize-it {
+    text-transform: capitalize;
+  }
+
+  .box {
+    width: 10rem;
+    height: 10rem;
+    background: #ccc;
+    margin: 1rem;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    border-radius: 5px;
+    padding: 1rem;
+  }
+
+  button {
+    cursor: pointer;
+  }
 </style>
 
-<Header {appName} />
-<main>
-  {#if page==='overview'}
-    {#if editMode === 'edit'}
-      <EditMeetup id={editedId} on:save-form-data={onSaveMeetup} on:cancel="{cancelEdit}" />
-    {/if}
-    <MeetupGrid meetups={$meetups} on:show-details="{showDetails}" on:edit-meetup="{onEditMeetup}"
-      on:add-meetup="{() => editMode = 'edit'}"/>
-  {:else}
-    <MeetupDetail id="{pageData.id}" on:close-details="{closeDetails}" />
-  {/if}
 
-</main>
+
---
+++ src/app-store.js
@@ -0,0 +1,30 @@
+// import { writable } from 'svelte/store';
+// for animation use 'tweened' or 'spring' instead of 'writable'. They support only date and numbers
+import { tweened } from 'svelte/motion';
+import { cubicIn } from 'svelte/easing';
+
+let beginingValue = 0;
+
+const progress = tweened(beginingValue, {
+  delay: 0,
+  duration: 700,
+  easing: cubicIn
+});
+
+setInterval(() => {
+  progress.set(0.5);
+}, 1500);
+
+setInterval(() => {
+  progress.set(1);
+}, 3000);
+
+setInterval(() => {
+  progress.set(0);
+}, 4500);
+
+const customProgress = {
+  subscribe: progress.subscribe
+};
+
+export default customProgress;
