b6a614c200cd8503d217d89c52b43bcdede20291
Anand raja <meetanandns@gmail.com>
2022-01-28 18:20:28 +0000

    Revert "animation complete"
    This reverts commit 7549ec421a5f94e081e7650401e1d4ac5cc76ba0.
    
    "wrong files removed"

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import App from './App.svelte';
 
 const app = new App({
-	// target: document.body
-  target: document.querySelector('#app'),
+	// target: document.body,
+  // target: document.querySelector('#app'),
+  target: document.getElementById('app'),
   props: {
-    appName: 'svelte animations!'
+    appName: 'infinity meetups'
   }
 });
 
